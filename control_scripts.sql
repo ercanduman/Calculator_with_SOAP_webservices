9ce1808ced96cfde577d46406a4623db95520abd
Ercan Duman✔️ <ercanduman30@gmail.com>
2017-10-16 11:26:21 +0300

    control script changed
     since retry_count logic changed then control script revised

--- control_scripts.sql
+++ control_scripts.sql
@@ -15,26 +15,13 @@ SELECT * FROM eduman.calculations ORDER BY calculation_id DESC;
 -- The executions log 
 SELECT * FROM eduman.calculations_wa_log ORDER BY log_id DESC;
 
--- the operations that need to be executed
+-- the operations that need to be executed 
 SELECT cal.calculation_id,
 			 cal.number_a,
 			 cal.number_b,
-			 (CASE
-				 WHEN cal.retry_count IS NULL THEN
-					(SELECT operation_retry_count FROM eduman.calculations_config)
-				 WHEN cal.retry_count IS NOT NULL THEN
-					cal.retry_count
-			 END) AS retry_count,
-			 (SELECT operand_name
-					FROM eduman.calculations_operand_config
-				 WHERE operand_id = cal.operand_id) AS operand_name
---  BULK COLLECT
---  INTO vt_Operations
-	FROM eduman.calculations cal
- WHERE (cal.status <> 'S' OR status IS NULL)
-	 AND (retry_count IS NULL OR retry_count > 0)
-		OR (retry_count > 0 AND xml_response IS NULL)
---AND cal.calculation_id = 10
-;
-AND(retry_count IS NULL) OR(retry_count > 0 AND xml_response IS NULL) OR xml_response LIKE '%NETWROK_ERROR%'
-
+			 NVL(cal.retry_count, 0) AS retry_count,
+			 conf.operand_name
+	FROM eduman.calculations cal, eduman.calculations_operand_config conf
+ WHERE cal.operand_id = conf.operand_id
+	 AND (cal.status <> 'S' OR cal.status IS NULL)
+	 AND (cal.retry_count < 3 OR cal.retry_count IS NULL);
